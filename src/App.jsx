3b5cdd7132c87b27e7c56b0f09fd83a2db20d243
Malcom Foxa <malcom.foca@gmail.com>
2025-04-10 18:17:34 -0300

    Agrego componente Navbar con sus estilos e implementacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,22 @@
-import './App.css'
-import { Perfumes } from './components/perfumes'
-import productList from "./products/productList.json"
+import "./App.css";
+import { Perfumes } from "./components/perfumes";
+import productList from "./products/productList.json";
 /* import noResults from "./products/noResults.json" */
+import Navbar from "./components/Navbar";
 
 function App() {
-  const perfumes = productList.Search
+  const perfumes = productList.Search;
 
   return (
-
-      <div className='page'>
-        <header>
-          <h1>Perfumes</h1>
-          <form className='form'>
-            <input placeholder='Perfumes Arabes' />
-            <button type="submit">Buscar</button>
-          </form>
-        </header>
-        <main>
-          <Perfumes perfumes={perfumes}/>
-        </main>
-      </div>
-    
-  )
+    <div className="page">
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <Perfumes perfumes={perfumes} />
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
