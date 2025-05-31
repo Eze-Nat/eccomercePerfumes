089f0b6a730b0f81416f65afcd23c5dccea1ba98
JulianBison <bisonjulian@gmail.com>
2025-05-31 09:28:43 -0300

    fetchpersonalizadosegundo arreglo

--- src/components/utils/customfetch.js
+++ src/components/utils/customfetch.js
@@ -1,20 +1,30 @@
-export const customfetch = (url,fetch_method,req_body, onSuccess, onError) => {
-    //${import.meta.env.VITE_BASE_SERVER_URL}
-    fetch(`localhost:3000/${url}`, {
-        headers: {
-            "Content-type": 'application/json'
-        },
-        method: `${fetch_method}`,
-        body: JSON.stringify(req_body)
-    })
-        .then(async res => {
-            if (!res.ok) {
-                const errData = await res.json();
-                throw new Error(errData.message?? "Hubo un error inesperado");
-            }
+export const customFetch = async (
+  url,
+  fetch_method,
+  req_body,
+  onSuccess,
+  onError
+) => {
+  try {
+    
+    const fullUrl = `${import.meta.env.VITE_BASE_SERVER_URL || 'http://localhost:3000'}/${url.replace(/^\//, '')}`;
+    
+    const response = await fetch(fullUrl, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: fetch_method, 
+      body: req_body ? JSON.stringify(req_body) : undefined, 
+    });
 
-            return res.json();
-        })
-        .then(onSuccess)
-        .catch(onError)
-}
+    if (!response.ok) {
+      const errData = await response.json();
+      throw new Error(errData.message ?? "Hubo un error inesperado");
+    }
+
+    const data = await response.json();
+    onSuccess(data);
+  } catch (error) {
+    onError(error);
+  }
+};
