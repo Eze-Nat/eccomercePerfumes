47af1fd851b09482bb4f0dcd931474776ef54a2c
JulianBison <bisonjulian@gmail.com>
2025-05-31 10:13:29 -0300

    agregado toastify

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import Register from "./components/auth/Register";
 import Dashboard from "./components/pages/dashboard/Dashboard.jsx";
 import CartPage from "./components/pages/cart/CartPage.jsx";
 import Navbar from "./components/pages/navbar/Navbar"; // Importamos Navbar
-
+import { ToastContainer } from "react-toastify"
 import "./App.css";
 
 function App() {
   return (
     <div className="gepa">
+      <ToastContainer />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
