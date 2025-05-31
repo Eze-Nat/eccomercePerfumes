9bbc93a32b4164f2560f8d6a044e4d78515a87af
JulianBison <bisonjulian@gmail.com>
2025-05-31 09:22:13 -0300

    customfetch

---
+++ src/components/utils/customfetch.js
@@ -0,0 +1,18 @@
+export const customfetch = (url,fetch_method,req_body, onSuccess, onError) => {
+    //${import.meta.env.VITE_BASE_SERVER_URL}
+    fetch(`localhost:3000/${url}`, {
+        headers: {
+            "Content-type": 'application/json'
+        },
+        method: `${fetch_method}`,
+        body: JSON.stringify(req_body)
+    })
+        .then(async res => {
+            if (!res.ok) {
+                const errData = await res.json();
+            }
+            return res.json();
+        })
+        .then(onSuccess)
+        .catch(onError)
+}
